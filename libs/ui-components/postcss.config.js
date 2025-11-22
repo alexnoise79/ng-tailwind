7dcf8c1bdf45a5f83e218602dd85e671c6386ddd
Alex Vitari <alexnoise79@gmail.com>
2025-11-22 12:32:52 +0100

    chore: update package dependencies and scripts for ui-components library, including migration to Angular 22 and Tailwind CSS integration

---
+++ libs/ui-components/postcss.config.js
@@ -0,0 +1,8 @@
+// PostCSS config for ng-packagr - minimal config without Tailwind
+// Tailwind styles are handled by the consuming application
+module.exports = {
+  plugins: {
+    autoprefixer: {}
+  }
+};
+
