7d4e6eb87627fb2835a16d6daeceb4b7774ded71
Alex Vitari <alexnoise79@gmail.com>
2025-11-23 13:33:42 +0100

    fix: update Cypress configuration files and results formatting
    - Added missing newline at the end of the Cypress configuration files for ui-demo and ui-demo-e2e.
    - Updated results.json to reflect new test durations and added a failed test case for the tooltip directive.

--- apps/ui-demo-e2e/cypress.config.js
+++ apps/ui-demo-e2e/cypress.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     ...nxE2EPreset(fileURLToPath(import.meta.url)),
     baseUrl: 'http://localhost:4201',
     supportFile: 'cypress/support/e2e.ts',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+  }
 });
