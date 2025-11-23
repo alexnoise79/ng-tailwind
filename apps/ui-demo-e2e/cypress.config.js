ff233fd36320ee3736c8f43f02d348fe4900893f
Alex Vitari <alexnoise79@gmail.com>
2025-11-23 13:23:36 +0100

    fix: stupid ide

--- apps/ui-demo-e2e/cypress.config.js
+++ apps/ui-demo-e2e/cypress.config.js
@@ -7,13 +7,6 @@ export default defineConfig({
     ...nxE2EPreset(fileURLToPath(import.meta.url)),
     baseUrl: 'http://localhost:4201',
     supportFile: 'cypress/support/e2e.ts',
-<<<<<<< HEAD
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
-
-=======
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
-  }
-});
->>>>>>> main
