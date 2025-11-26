d8a9c1165e8280bb3c380cc470d5f74b0c2acb40
Alex Vitari <alexnoise79@gmail.com>
2025-11-26 23:08:30 +0100

    refactor: replace provideAnimations with universalProviders for improved SSR support

--- apps/ui-demo/src/main.ts
+++ apps/ui-demo/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { universalProviders } from '@universal/universal.providers';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), provideAnimations()]
+  providers: [provideRouter(appRoutes), universalProviders()]
 }).catch(err => console.error(err));
