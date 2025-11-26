76ca5c10c6812f73a3dfea26449cd699bb017719
Alex Vitari <alexnoise79@gmail.com>
2025-11-26 23:01:44 +0100

    feat(universal): create index file to export key utilities for SSR and storage management

---
+++ libs/universal/index.ts
@@ -0,0 +1,4 @@
+export { WINDOW, WindowService } from './window.service';
+export { LocalStorage, SessionStorage, getLocalStorage, getSessionStorage, universalProviders } from './universal.providers';
+export { SsrRenderDirective } from './ssr-render.directive';
+
