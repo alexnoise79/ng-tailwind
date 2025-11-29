5dcfe2b955139d43eced900654facf23a302f54b
Alex Vitari <alexnoise79@gmail.com>
2025-11-29 21:53:05 +0100

    refactor(components): update SVG paths for alert and toast components for consistency

--- libs/ui-components/src/components/toast/toast.component.ts
+++ libs/ui-components/src/components/toast/toast.component.ts
@@ -134,10 +134,10 @@ export class NgtToast implements OnInit, OnDestroy {
       return this.iconValue();
     }
     const icons: Record<Variant, string> = {
-      success: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+      success: 'M16 2l-9 9.254-4.229-4.009-2.971 2.973 7.2 6.982 12-12.228z',
       info: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
       warning: 'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z',
-      danger: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+      danger: 'M6 18L18 6M6 6l12 12',
       primary: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
       secondary: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
       light: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
